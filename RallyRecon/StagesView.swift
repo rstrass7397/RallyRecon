f884d8eddc6ab51cd478f543b85096ecfcd18b95
Flaminkko <olizdu123@gmail.com>
2025-01-27 10:57:57 -0600

    made a basic textfield/list input thing

--- RallyRecon/StagesView.swift
+++ RallyRecon/StagesView.swift
@@ -5,3 +5,28 @@
 //  Created by Olivier Zdunczyk on 1/27/25.
 //
 
+import SwiftUI
+
+struct StagesView: View {
+    @State var inputText: String = ""
+    @State var items: [String] = []
+    var body: some View {
+        TextField("Enter text", text: $inputText, onCommit: addItem)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+
+        List(items, id: \.self) { item in
+            Text(item)
+        }
+
+    }
+    func addItem() {
+            guard !inputText.isEmpty else { return }
+            items.append(inputText)
+            inputText = ""
+        }
+}
+
+#Preview {
+    StagesView()
+}
