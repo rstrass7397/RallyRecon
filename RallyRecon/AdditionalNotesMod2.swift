bd35d2a69624e3c3170e96658976881de6d60ea1
Amar <asetka7368@stu.d214.org>
2025-01-29 10:27:55 -0600

    added all the code into the new project

--- RallyRecon/AdditionalNotesMod2.swift
+++ RallyRecon/AdditionalNotesMod2.swift
@@ -10,94 +10,99 @@ import SwiftUI
 struct AdditionalNotesMod2: View {
     @State var isTrueNotes = ["SLP": false, "RUF": false, "Stay L": false, "Stay R": false, "Thru": false, "!": false, "!!": false, "!!!": false]
     var body: some View {
-        
-        
-        HStack {
-            VStack(alignment: .center , spacing: 30) {
-                
-                Button {
-                    isTrueNotes["SLP"]!.toggle()
-                } label: {
-                    Text("SLP")
-                        .frame(width: 300, height: 300)
-                        .foregroundColor(.white)
-                        .font(.system(size:60, weight: .bold))
-                        .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["SLP"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
-                    
-                }
-                Button {
-                    isTrueNotes["RUF"]!.toggle()
-                } label: {
-                    Text("RUF")
-                        .frame(width: 300, height: 300)
-                        .foregroundColor(.white)
-                        .font(.system(size:60, weight: .bold))
-                        .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["RUF"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
-                    
-                }
-                Button {
-                    isTrueNotes["Stay L"]!.toggle()
-                } label: {
-                    Text("Stay L")
-                        .frame(width: 300, height: 300)
-                        .foregroundColor(.white)
-                        .font(.system(size:60, weight: .bold))
-                        .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["Stay L"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
-                    
-                }
-                Button {
-                    isTrueNotes["Stay R"]!.toggle()
-                } label: {
-                    Text("Stay R")
-                        .frame(width: 300, height: 300)
-                        .foregroundColor(.white)
-                        .font(.system(size:60, weight: .bold))
-                        .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["Stay R"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
-                }
-            }
+        ZStack{
+            Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea()
             
-            VStack(alignment: .center , spacing: 30) {
+            
+            HStack {
+                VStack(alignment: .center , spacing: 30) {
+                    
+                    Button {
+                        isTrueNotes["SLP"]!.toggle()
+                    } label: {
+                        Text("SLP")
+                            .frame(width: 200, height: 200)
+                            .foregroundColor(.white)
+                            .font(.system(size:60, weight: .bold))
+                            .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["SLP"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
+                        
+                    }
+                    Button {
+                        isTrueNotes["RUF"]!.toggle()
+                    } label: {
+                        Text("RUF")
+                            .frame(width: 200, height: 200)
+                            .foregroundColor(.white)
+                            .font(.system(size:60, weight: .bold))
+                            .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["RUF"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
+                        
+                    }
+                    Button {
+                        isTrueNotes["Stay L"]!.toggle()
+                    } label: {
+                        Text("Stay L")
+                            .frame(width: 200, height: 200)
+                            .foregroundColor(.white)
+                            .font(.system(size:60, weight: .bold))
+                            .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["Stay L"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
+                        
+                    }
+                    Button {
+                        isTrueNotes["Stay R"]!.toggle()
+                    } label: {
+                        Text("Stay R")
+                            .frame(width: 200, height: 200)
+                            .foregroundColor(.white)
+                            .font(.system(size:60, weight: .bold))
+                            .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["Stay R"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
+                    }
+                }
                 
-                Button {
-                    isTrueNotes["Thru"]!.toggle()
-                } label: {
+                VStack(alignment: .center , spacing: 30) {
                     
-                    Text("Thru")
-                        .frame(width: 300, height: 300)
-                        .foregroundColor(.white)
-                        .font(.system(size:60, weight: .bold))
-                        .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["Thru"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
+                    Button {
+                        isTrueNotes["Thru"]!.toggle()
+                    } label: {
+                        
+                        Text("Thru")
+                            .frame(width: 200, height: 200)
+                            .foregroundColor(.white)
+                            .font(.system(size:60, weight: .bold))
+                            .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["Thru"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
+                        
+                    }
+                    Button {
+                        isTrueNotes["!"]!.toggle()
+                    } label: {
+                        Text("!")
+                            .frame(width: 200, height: 200)
+                            .foregroundColor(.white)
+                            .font(.system(size:60, weight: .bold))
+                            .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["!"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
+                        
+                    }
+                    Button {
+                        isTrueNotes["!!"]!.toggle()
+                    } label: {
+                        Text("!!")
+                            .frame(width: 200, height: 200)
+                            .foregroundColor(.white)
+                            .font(.system(size:60, weight: .bold))
+                            .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["!!"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
+                    }
                     
-                }
-                Button {
-                    isTrueNotes["!"]!.toggle()
-                } label: {
-                    Text("!")
-                        .frame(width: 300, height: 300)
-                        .foregroundColor(.white)
-                        .font(.system(size:60, weight: .bold))
-                        .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["!"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
-                    
-                }
-                Button {
-                    isTrueNotes["!!"]!.toggle()
-                } label: {
-                    Text("!!")
-                        .frame(width: 300, height: 300)
-                        .foregroundColor(.white)
-                        .font(.system(size:60, weight: .bold))
-                        .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["!!"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
-                }
-                
-                Button {
-                    isTrueNotes["!!!"]!.toggle()
-                } label: {
-                    Text("!!!")
-                        .frame(width: 300, height: 300)
-                        .foregroundColor(.white)
-                        .font(.system(size:60, weight: .bold))
-                        .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["!!!"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
-                    
+                    Button {
+                        isTrueNotes["!!!"]!.toggle()
+                    } label: {
+                        Text("!!!")
+                            .frame(width: 200, height: 200)
+                            .foregroundColor(.white)
+                            .font(.system(size:60, weight: .bold))
+                            .background(RoundedRectangle(cornerRadius: 30).foregroundColor(isTrueNotes["!!!"]! ? .blue : Color(red: 17 / 255, green: 51 / 255, blue: 95/255)))
+                        
+                    }
                 }
             }
         }
@@ -107,4 +112,3 @@ struct AdditionalNotesMod2: View {
 #Preview {
     AdditionalNotesMod2()
 }
-
--- RallyRecon/StraightsMod.swift
+++ RallyRecon/StraightsMod.swift
@@ -150,17 +150,6 @@ struct StraightsMod: View {
                 }
                     
                 }
-                
-                
-                HStack{
-                    NavigationLink("Next", destination: CrestsMod())
-                        .frame(width: 300, height: 80)
-                        .background(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
-                        .cornerRadius(20)
-                        .font(.system(size:40, weight: .bold))
-                        .foregroundColor(Color(red: 248 / 255, green: 248 / 255, blue: 238/255))
-                        .padding(60)
-                }
             }
            
         }
