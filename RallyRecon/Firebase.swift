8b2884e8f488e5afd5423e66d67154b6faea1efa
Andy Maru <Andymaru17@gmail.com>
2025-02-06 11:21:30 -0600

    I created a model that creates a class of all the different modifiers for a turn such as the sharpness, left or right, and any other information about the specific turn. I am having an error but I decided to commit the code I have so far.

--- RallyRecon/Firebase.swift
+++ RallyRecon/Firebase.swift
@@ -5,6 +5,36 @@
 //  Created by Ryan S. Strass on 1/31/25.
 //
 
+import SwiftData
+import SwiftUI
+
 struct Test: Decodable, Hashable {
     var name: String
+
+@Model
+class Rally: Hashable {
+    var turnDirection: String
+    var turnSharpness: Int
+    var turnDuration: String
+    var turnStraights: Int
+    var turnBumps: String
+    var turnCrests: String
+    var turnAdditionalNotes: String
+    var turnAdditionalNotes2: String
+        
+    init(turnDirection: String, turnSharpness: Int, turnDuration: String, turnStraights: Int, turnBumps: String, turnCrests: String, turnAdditional: String, turnAdditional2: String) {
+        self.turnDirection = turnDirection
+        self.turnSharpness = turnSharpness
+        self.turnDuration = turnDuration
+        self.turnStraights = turnStraights
+        self.turnBumps = turnBumps
+        self.turnCrests = turnCrests
+        self.turnAdditionalNotes = turnAdditionalNotes
+        self.turnAdditionalNotes2 = turnAdditionalNotes2
+      
+
+        
+    }
+}
+
 }
