4ed1af87302802dcad8139b16698812ed126cb02
Ryan Strass <rstrass7397@stu.d214.org>
2025-02-06 11:00:59 -0600

    added a new view for both stages and rallies

--- RallyRecon/CreateAStage.swift
+++ RallyRecon/CreateAStage.swift
@@ -1,25 +1,24 @@
 //
-//  CreateARally.swift
+//  CreateAStage.swift
 //  RallyRecon
 //
-//  Created by Olivier Zdunczyk on 12/11/24.
+//  Created by Ryan S. Strass on 2/6/25.
 //
-
 import SwiftUI
 
-struct CreateARally: View {
-    @State var RallyName = ""
+struct CreateAStage: View {
+    @State var StageName = ""
     var body: some View {
         ZStack{
             Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .ignoresSafeArea()
             VStack{
-                Text("Enter The Rally Name")
+                Text("Enter The Stage Name")
                     .font(.system(size:75, weight: .bold))
                     .padding(30)
                     .foregroundColor(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
-                TextField("Enter Rally Name", text: $RallyName)
+                TextField("Enter Stage Name", text: $StageName)
                     .padding()
                     .textFieldStyle(.roundedBorder)
                     .foregroundColor(.black)
