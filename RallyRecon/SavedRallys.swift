fad26d3220d0513619b47b50768ad73e0a04ccba
Amar <asetka7368@stu.d214.org>
2025-02-06 10:38:52 -0600

    Added the edit button to the saved rallies

--- RallyRecon/SavedRallys.swift
+++ RallyRecon/SavedRallys.swift
@@ -6,7 +6,22 @@ struct SavedRallys : View {
             Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .ignoresSafeArea()
+            Button{
+                
+            } label: {
+               Text("Edit")
+                    .frame(width: 300, height: 80)
+                    .background(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
+                    .cornerRadius(20)
+                    .font(.system(size:40, weight: .bold))
+                    .foregroundColor(Color(red: 248 / 255, green: 248 / 255, blue: 238/255))
+                
+                   
+            }
         }
     }
+}
+#Preview {
+    SavedRallys()
 }
 
