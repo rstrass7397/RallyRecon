9de3e1ddc4e7d26ef1700fd1067fd99f451ea90d
Amar <asetka7368@stu.d214.org>
2025-02-10 11:16:26 -0600

    made savedrallys sync up with stagesview and the modifier hub when you edit

--- RallyRecon/SavedRallys.swift
+++ RallyRecon/SavedRallys.swift
@@ -6,17 +6,29 @@ struct SavedRallys : View {
             Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .ignoresSafeArea()
-            Button{
+            
+            VStack{
+                Text("Saved Rallies")
+                    .font(.system(size:75, weight: .bold))
+                    .padding(30)
+                    .foregroundColor(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
                 
-            } label: {
-               Text("Edit")
+                
+                NavigationLink("View Saved Stages", destination: StagesView())
+                    .frame(width: 400, height: 80)
+                    .background(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
+                    .cornerRadius(20)
+                    .font(.system(size:40, weight: .bold))
+                    .foregroundColor(Color(red: 248 / 255, green: 248 / 255, blue: 238/255))
+                    .padding(30)
+                
+                NavigationLink("Edit", destination: ModifierHub())
                     .frame(width: 300, height: 80)
                     .background(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
                     .cornerRadius(20)
                     .font(.system(size:40, weight: .bold))
                     .foregroundColor(Color(red: 248 / 255, green: 248 / 255, blue: 238/255))
-                
-                   
+                    .padding(30)
             }
         }
     }
--- RallyRecon/StagesView.swift
+++ RallyRecon/StagesView.swift
@@ -4,57 +4,57 @@
 ////
 ////  Created by Olivier Zdunczyk on 1/27/25.
 ////
-//import Firebase
-//import SwiftUI
-//import FirebaseFirestore
-//
-//struct StagesView: View {
-//    @FirestoreQuery(collectionPath: "Test") var tests: [Test]
-//    @State var inputText: String = ""
-//    @State var items: [String] = []
-//    @State var searchText = ""
-//    var searchResults: [Test] {
-//            if searchText.isEmpty {
-//                return tests
-//            } else {
-//                return tests.filter { item in item.name.contains(searchText) }
-//            }
-//        }
-//    var body: some View {
-//        NavigationStack {
-//            TextField("Enter text", text: $inputText, onCommit: addItem
-//            )
-//                .textFieldStyle(RoundedBorderTextFieldStyle())
-//                .padding()
-//
-//            List(searchResults, id: \.self) { item in
-//                NavigationLink {
-//                    Text(item.name)
-//                    List{
-//                        Text("1")
-//                        Text("2")
-//                        Text("3")
-//                    }
-//                } label: {
-//                    Text(item.name)
-//                }
-//            }
-//            .navigationTitle("Rallies")
-//        }
-//        .searchable(text: $searchText)
-//
-//    }
-//        func addItem() {
-//            let newTest = Test(name: inputText)
-//            DispatchQueue.main.async {
-//                inputText = ""
-//            }
-//            let dataBase = Firestore.firestore()
-//            dataBase.collection("Test").document(newTest.name).setData(["name":newTest.name])
-//        }
-//    }
-//
-//#Preview {
-//    StagesView()
-//}
-//
+import Firebase
+import SwiftUI
+import FirebaseFirestore
+
+struct StagesView: View {
+    @FirestoreQuery(collectionPath: "Test") var tests: [Test]
+    @State var inputText: String = ""
+    @State var items: [String] = []
+    @State var searchText = ""
+    var searchResults: [Test] {
+            if searchText.isEmpty {
+                return tests
+            } else {
+                return tests.filter { item in item.name.contains(searchText) }
+            }
+        }
+    var body: some View {
+        NavigationStack {
+            TextField("Enter text", text: $inputText, onCommit: addItem
+            )
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            List(searchResults, id: \.self) { item in
+                NavigationLink {
+                    Text(item.name)
+                    List{
+                        Text("1")
+                        Text("2")
+                        Text("3")
+                    }
+                } label: {
+                    Text(item.name)
+                }
+            }
+            .navigationTitle("Rallies")
+        }
+        .searchable(text: $searchText)
+
+    }
+        func addItem() {
+            let newTest = Test(name: inputText)
+            DispatchQueue.main.async {
+                inputText = ""
+            }
+            let dataBase = Firestore.firestore()
+            dataBase.collection("Test").document(newTest.name).setData(["name":newTest.name])
+        }
+    }
+
+#Preview {
+    StagesView()
+}
+
