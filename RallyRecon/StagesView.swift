f7ccca365ed47c05dab0e13270df20ddb6fec844
Flaminkko <olizdu123@gmail.com>
2025-03-17 10:55:15 -0500

    Fixed the list in the stages view. Added a way to delete items from the list

--- RallyRecon/StagesView.swift
+++ RallyRecon/StagesView.swift
@@ -11,44 +11,58 @@ struct StagesView: View {
     @State var inputText: String = ""
     @State var items = ["Item 1", "Item 2"]
     @State var editingIndex: Int? = nil
-        @State var editedText: String = ""
+    @State var editedText: String = ""
+
     var body: some View {
         NavigationStack {
-            TextField("Enter text", text: $inputText
-            )
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .padding()
-            .font(.title)
-            
-            NavigationLink("View Saved Rallies", destination: SavedRallys())
-                .foregroundColor(.black)
-            
-            List { ForEach(0..<items.count, id: \.self) { index in
-                
-                if editingIndex == index {
-            TextField("Edit Item", text: $editedText)
+            TextField("Enter text", text: $inputText)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
-            Button("Save") {
-                items[index] = editedText
-                editingIndex = nil
+                .font(.title)
+
+            NavigationLink("View Saved Rallies", destination: SavedRallys())
+                .foregroundColor(.black)
+
+            List {
+                ForEach(0..<items.count, id: \.self) { index in
+                    if editingIndex == index {
+                        VStack {
+                            TextField("Edit Item", text: $editedText)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .padding()
+
+                            Button("Save") {
+                                items[index] = editedText
+                                editingIndex = nil
+                            }
+                            .padding(.trailing)
+                        }
+                    } else {
+                        HStack {
+                            Text(items[index])
+                            Spacer()
+                            Button("Edit") {
+                                editingIndex = index
+                                editedText = items[index]
+                            }
+                            .font(.title2)
+                        }
                     }
-                .padding(.trailing)
-                } else {
-            Text(items[index])
-            Button("Edit") {
-            editingIndex = index
-            editedText = items[index]
-                                       }
-                        .font(.title)
-                
+                }
+                .onDelete(perform: deleteItem)
             }
             .navigationTitle("Stages")
+            .toolbar {
+                EditButton()
+            }
+            .font(.title)
         }
-        .font(.title)
     }
+    private func deleteItem(at offsets: IndexSet) {
+            items.remove(atOffsets: offsets)
+        }
 }
+
 #Preview {
     StagesView()
 }
-
