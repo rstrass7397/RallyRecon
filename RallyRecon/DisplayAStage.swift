0da7da82e75cc9dffdf4e06cf15b12e5ca70f246
Ryan Strass <rstrass7397@stu.d214.org>
2025-03-19 10:18:51 -0500

    fixed the list - working on navigation link

--- RallyRecon/DisplayAStage.swift
+++ RallyRecon/DisplayAStage.swift
@@ -8,18 +8,27 @@
 import SwiftUI
 
 struct DisplayAStage : View {
-    @State var addStage : String = ""
+    @State var addStage: String = ""
     @Binding var currentRally: String
-    let stages = [""]
+    @State var stages: [String] = []
     var body: some View {
         Text("\(currentRally)")
         TextField("Add a stage", text: $addStage)
             .textFieldStyle(RoundedBorderTextFieldStyle())
             .padding()
             .font(.title)
+            .onSubmit {
+                stages.append(addStage)
+                addStage = ""
+            }
         List {
             ForEach(stages,id: \.self){ stage  in
-                Text(stage)
+                NavigationLink {
+                    Text(stage)
+                    Text("hello")
+                    } label: {
+                        Text(stage)
+                    }
             }
         }
         .frame(width: 800, height: 900)
