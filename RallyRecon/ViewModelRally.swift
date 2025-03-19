b4aa724bec0e3e1a1ea15a778d807b9d6a2b3db0
Andy Maru <Andymaru17@gmail.com>
2025-03-19 11:06:02 -0500

    I created a new class that manages a list of PaceNotes and allows for updates to individual features.

--- RallyRecon/ViewModelRally.swift
+++ RallyRecon/ViewModelRally.swift
@@ -6,7 +6,22 @@
 //
 
 import SwiftUI
+import SwiftData
 
-//class RallyViewModel: ObservableObject {
-//    @Published var rallies: [Rally] = []
-//}
+class PaceNotesViewModel: ObservableObject {
+    @Published var paceNotesList: [PaceNotes] = []
+    
+    func updatePaceNote(_ paceNote: PaceNotes, newDirection: String, newSharpness: Int, newDuration: String, newStraights: Int, newBumps: String, newCrests: String, newNotes: String, newNotes2: String) {
+        
+        if let index = paceNotesList.firstIndex(where: { $0.id == paceNote.id }) {
+            paceNotesList[index].turnDirection = newDirection
+            paceNotesList[index].turnSharpness = newSharpness
+            paceNotesList[index].turnDuration = newDuration
+            paceNotesList[index].turnStraights = newStraights
+            paceNotesList[index].turnBumps = newBumps
+            paceNotesList[index].turnCrests = newCrests
+            paceNotesList[index].turnAdditionalNotes = newNotes
+            paceNotesList[index].turnAdditionalNotes2 = newNotes2
+        }
+    }
+}
