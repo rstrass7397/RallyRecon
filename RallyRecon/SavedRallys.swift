96f849fe1edbdf70b030da84c57867715729a96a
Andy Maru <Andymaru17@gmail.com>
2025-03-21 11:17:33 -0500

    Deleted a few unecessary things.

--- RallyRecon/SavedRallys.swift
+++ RallyRecon/SavedRallys.swift
@@ -11,17 +11,10 @@ struct SavedRallies: View {
                     .padding(30)
                     .foregroundColor(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
                 
-                
-                
-//                List(rallies, id: \.self) { rally in
-//                    Text(rally.name)
-//                        .font(.title)
-//                }
-//                .navigationTitle("Stages")
             }
             .font(.title)
-            .frame(maxWidth: .infinity, maxHeight: .infinity) // Ensure background fills entire screen
-                       .background(Color(red: 248 / 255, green: 248 / 255, blue: 238 / 255))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(red: 248 / 255, green: 248 / 255, blue: 238 / 255))
         }
     }
 
