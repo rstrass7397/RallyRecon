7d8e700cdb2e7ce4058c21410af6aad44ba8bbd5
Amar <asetka7368@stu.d214.org>
2025-05-06 11:02:51 -0500

    added the colors were going to use

--- RallyRecon/RallyListView.swift
+++ RallyRecon/RallyListView.swift
@@ -8,16 +8,25 @@ struct RallyListView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text("Rallies")
-                    .font(.largeTitle)
-                    .padding()
-                
-                List(rallies, id: \.name) { rally in
-                    NavigationLink(destination: StageListView(rally: rally)) {
-                        Text(rally.name)
+                ZStack{
+                    Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .ignoresSafeArea()
+                    Text("Rallies")
+                        .padding()
+                        .frame(width: 150, height: 80)
+                        .background(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
+                        .cornerRadius(20)
+                        .font(.system(size:35, weight: .bold))
+                        .foregroundColor(Color(red: 248 / 255, green: 248 / 255, blue: 238/255))
+                    
+                    List(rallies, id: \.name) { rally in
+                        NavigationLink(destination: StageListView(rally: rally)) {
+                            Text(rally.name)
+                        }
                     }
+                    .navigationBarItems(trailing: NavigationLink("Add Rally", destination: AddRallyView(rallies: $rallies)))
                 }
-                .navigationBarItems(trailing: NavigationLink("Add Rally", destination: AddRallyView(rallies: $rallies)))
             }
         }
     }
