f49e20701f0782edfc0fee1e1f4066143e7b3880
Andy Maru <Andymaru17@gmail.com>
2025-05-16 10:59:09 -0500

    Deleted SheetView and other unecessary code. Changed color scheme for RallyListView.

--- RallyRecon/RallyListView.swift
+++ RallyRecon/RallyListView.swift
@@ -5,32 +5,39 @@ struct RallyListView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Rallies")
-                    .font(.largeTitle)
-                    .padding()
+            ZStack{
+                Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .ignoresSafeArea()
+            }
                 
-                List(rallies, id: \.name) { rally in
-                    NavigationLink(destination: StageListView(rally: rally, stages: $rallies.first(where: { $0.id == rally.id })!.stages)) {
-                        Text(rally.name)
-                        ZStack{
-                            Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
-                                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                                .ignoresSafeArea()
-                            Text("Rallies")
-                                .padding()
-                                .frame(width: 150, height: 60)
-                                .background(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
-                                .cornerRadius(20)
-                                .font(.system(size:25, weight: .bold))
-                                .foregroundColor(Color(red: 248 / 255, green: 248 / 255, blue: 238/255))
-                            
-                            List(rallies, id: \.name) { rally in
-                                NavigationLink(destination: StageListView(rally: rally)) {
-                                    Text(rally.name)
+                VStack {
+                    Text("Rallies")
+                        .font(.largeTitle)
+                        .padding()
+                    
+                    List(rallies, id: \.name) { rally in
+                        NavigationLink(destination: StageListView(rally: rally, stages: $rallies.first(where: { $0.id == rally.id })!.stages)) {
+                            Text(rally.name)
+                            ZStack{
+                                Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                    .ignoresSafeArea()
+                                Text("Rallies")
+                                    .padding()
+                                    .frame(width: 150, height: 60)
+                                    .background(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
+                                    .cornerRadius(20)
+                                    .font(.system(size:25, weight: .bold))
+                                    .foregroundColor(Color(red: 248 / 255, green: 248 / 255, blue: 238/255))
+                                
+                                List(rallies, id: \.name) { rally in
+                                    NavigationLink(destination: StageListView)
+                                    Text(Rally.name)
+                                    }
                                 }
+                                .navigationBarItems(trailing: NavigationLink("Add Rally", destination: AddRallyView(rallies: $rallies)))
                             }
-                            .navigationBarItems(trailing: NavigationLink("Add Rally", destination: AddRallyView(rallies: $rallies)))
                         }
                     }
                 }
