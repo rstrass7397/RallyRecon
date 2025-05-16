511be84c8c051f20db06a53d15b1822d3dab3fb8
Ryan Strass <rstrass7397@stu.d214.org>
2025-05-16 10:54:32 -0500

    fixed the errors be returning to previous version

--- RallyRecon/RallyListView.swift
+++ RallyRecon/RallyListView.swift
@@ -13,29 +13,10 @@ struct RallyListView: View {
                 List(rallies, id: \.name) { rally in
                     NavigationLink(destination: StageListView(rally: rally, stages: $rallies.first(where: { $0.id == rally.id })!.stages)) {
                         Text(rally.name)
-                ZStack{
-                    Color(red: 248 / 255, green: 248 / 255, blue: 238/255)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .ignoresSafeArea()
-                    Text("Rallies")
-                        .padding()
-                        .frame(width: 150, height: 80)
-                        .background(Color(red: 17 / 255, green: 51 / 255, blue: 95/255))
-                        .cornerRadius(20)
-                        .font(.system(size:35, weight: .bold))
-                        .foregroundColor(Color(red: 248 / 255, green: 248 / 255, blue: 238/255))
-                    
-                    List(rallies, id: \.name) { rally in
-                        NavigationLink(destination: StageListView(rally: rally)) {
-                            Text(rally.name)
-                        }
                     }
-                    .navigationBarItems(trailing: NavigationLink("Add Rally", destination: AddRallyView(rallies: $rallies)))
                 }
+                .navigationBarItems(trailing: NavigationLink("Add Rally", destination: AddRallyView(rallies: $rallies))) // Pass the Binding here
             }
         }
     }
-}
-#Preview {
-    RallyListView()
 }
