a8cbe929ec913b101f66859c1127c467dce8a6d2
Andy Maru <Andymaru17@gmail.com>
2025-05-20 10:07:31 -0500

    Created HomePage as the first View that pops up in the app instead of RallyListView.

--- RallyRecon/RallyReconApp.swift
+++ RallyRecon/RallyReconApp.swift
@@ -6,7 +6,7 @@ import SwiftUI
 struct RallyReconApp: App {
     var body: some Scene {
         WindowGroup {
-            RallyListView()
+            HomeView()
         }
     }
 }
--- RallyRecon/RallySheetView.swift
+++
@@ -1,6 +0,0 @@
-//
-//  RallySheetView.swift
-//  RallyRecon
-//
-//  Created by Ananya A. Maru on 5/2/25.
-//
