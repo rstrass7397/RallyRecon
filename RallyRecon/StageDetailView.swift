1e226c6735e1c60a6d15252dcb2bba9a33c56941
Amar <asetka7368@stu.d214.org>
2025-05-27 10:56:40 -0500

    made the modifier page look better

--- RallyRecon/StageDetailView.swift
+++ RallyRecon/StageDetailView.swift
@@ -2,44 +2,50 @@ import SwiftUI
 
 struct StageDetailView: View {
     @EnvironmentObject var rallyManager: RallyManager
-
+    
     let rallyID: UUID
     let stageID: UUID
-
+    
     var rallyIndex: Int? {
         rallyManager.rallies.firstIndex(where: { $0.id == rallyID })
     }
-
+    
     var stageIndex: Int? {
         if let rallyIndex = rallyIndex {
             return rallyManager.rallies[rallyIndex].stages.firstIndex(where: { $0.id == stageID })
         }
         return nil
     }
-
+    
     @State private var showTurnInfo = false
     @State private var isTrueTurns: [String: Bool] = [
         "left": false, "right": false,
         "1": false, "2": false, "3": false,
         "4": false, "5": false, "6": false
     ]
-
+    
     var body: some View {
+        ZStack{
+            Color(red: 248 / 255, green: 248 / 255, blue: 238 / 255)
+                .ignoresSafeArea()
         VStack {
             if let rallyIndex = rallyIndex, let stageIndex = stageIndex {
                 Text(rallyManager.rallies[rallyIndex].stages[stageIndex].name)
                     .font(.largeTitle)
                     .padding()
-
+                
                 List {
                     ForEach(rallyManager.rallies[rallyIndex].stages[stageIndex].trueModifiers, id: \.self) { modifier in
                         Text(modifier)
+                            .foregroundColor(.navy)
                     }
                     .onDelete { offsets in
                         rallyManager.deleteModifier(atOffsets: offsets, rallyID: rallyID, stageID: stageID)
                     }
                 }
-
+                .scrollContentBackground(.hidden)
+                .background(Color.creme)
+                
                 Button("Add Modifier") {
                     showTurnInfo.toggle()
                 }
@@ -57,6 +63,7 @@ struct StageDetailView: View {
             }
         }
     }
+}
 
     private func resetSelections() {
         for key in isTrueTurns.keys {
