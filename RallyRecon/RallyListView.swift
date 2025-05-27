836e80a7478753442e9eeeddd1e82c5264e42645
Ryan Strass <rstrass7397@stu.d214.org>
2025-05-27 11:15:45 -0500

    fixed errors

--- RallyRecon/RallyListView.swift
+++ RallyRecon/RallyListView.swift
@@ -2,13 +2,13 @@ import SwiftUI
 
 struct RallyListView: View {
     @EnvironmentObject var rallyManager: RallyManager
-
+    
     var body: some View {
         NavigationStack {
             ZStack {
                 Color.creme
                     .ignoresSafeArea()
-
+                
                 VStack(alignment: .leading, spacing: 20) {
                     Text("Rallies")
                         .font(.system(size: 36, weight: .bold))
@@ -34,5 +34,5 @@ struct RallyListView: View {
             }
         }
     }
-
+}
 
--- RallyRecon/StageDetailView.swift
+++ RallyRecon/StageDetailView.swift
@@ -1,25 +1,22 @@
 import SwiftUI
-
 struct StageDetailView: View {
     @EnvironmentObject var rallyManager: RallyManager
-
+    
     let rallyID: UUID
     let stageID: UUID
-
+    
     var rallyIndex: Int? {
         rallyManager.rallies.firstIndex(where: { $0.id == rallyID })
     }
-
+    
     var stageIndex: Int? {
         if let rallyIndex = rallyIndex {
             return rallyManager.rallies[rallyIndex].stages.firstIndex(where: { $0.id == stageID })
         }
         return nil
     }
+    
     var body: some View {
-        ZStack{
-            Color(red: 248 / 255, green: 248 / 255, blue: 238 / 255)
-                .ignoresSafeArea()
         VStack {
             if let rallyIndex = rallyIndex, let stageIndex = stageIndex {
                 Text(rallyManager.rallies[rallyIndex].stages[stageIndex].name)
@@ -29,33 +26,17 @@ struct StageDetailView: View {
                 List {
                     ForEach(rallyManager.rallies[rallyIndex].stages[stageIndex].trueModifiers, id: \.self) { modifier in
                         Text(modifier)
-                            .foregroundColor(.navy)
                     }
                     .onDelete { offsets in
                         rallyManager.deleteModifier(atOffsets: offsets, rallyID: rallyID, stageID: stageID)
                     }
                 }
-                .scrollContentBackground(.hidden)
-                .background(Color.creme)
                 
-                Button("Add Modifier") {
-                    showTurnInfo.toggle()
-                }
-                .padding()
-                .buttonStyle(.bordered)
-                .sheet(isPresented: $showTurnInfo) {
-                    TurnInfoMod(isTrueTurns: $isTrueTurns) { selectedModifier in
-                        let modifierString = "\(selectedModifier[0])"
-                        rallyManager.addModifier(to: rallyID, stageID: stageID, modifier: modifierString)
-                        resetSelections()
-                    }
-                }
-
                 NavigationLink(
                     "Add Modifiers",
                     destination: ModHub(rallyID: rallyID, stageID: stageID)
                 )
-
+                
             } else {
                 Text("Stage not found")
             }
