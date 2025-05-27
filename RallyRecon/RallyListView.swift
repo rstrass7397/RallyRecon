212564632402be3b44c6815e92ab377f0d95ea4c
Andy Maru <Andymaru17@gmail.com>
2025-05-27 11:00:51 -0500

    Added clearer RallyListView Layout and added a title for clarity.

--- RallyRecon/RallyListView.swift
+++ RallyRecon/RallyListView.swift
@@ -5,18 +5,33 @@ struct RallyListView: View {
 
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(rallyManager.rallies) { rally in
-                    NavigationLink(destination: StageListView(rallyID: rally.id)) {
-                        Text(rally.name)
+            ZStack {
+                Color.creme
+                    .ignoresSafeArea()
+
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Rallies")
+                        .font(.system(size: 36, weight: .bold))
+                        .foregroundColor(.navy)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(.top, 20)
+                    List {
+                        ForEach(rallyManager.rallies) { rally in
+                            NavigationLink(destination: StageListView(rallyID: rally.id)) {
+                                Text(rally.name)
+                                    .font(.title3)
+                                    .foregroundColor(.navy)
+                            }
+                        }
+                        .onDelete(perform: rallyManager.deleteRally)
                     }
+                    .scrollContentBackground(.hidden)
                 }
-                .onDelete(perform: rallyManager.deleteRally)
             }
-            .navigationTitle("Rallies")
             .toolbar {
                 NavigationLink("Add Rally", destination: AddRallyView())
             }
         }
     }
 }
+
